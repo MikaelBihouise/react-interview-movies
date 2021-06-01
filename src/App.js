e57c101c83444e098455487cc2a231510a18c4bf
Mikael B <mikaelbihouise@hotmail.fr>
2021-06-01 03:21:14 +0200

    useEffect to get movie list and first mapping

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import logo from './images/logo.svg';
 import CardMovie from './components/CardMovie';
+let movies = require('./components/movies.js');
 
 function App() {
+
+  const [movieList, setMovieList] = useState([])
+
+  useEffect(() => {
+    async function loadData() {
+      const response = await movies;
+      let result = response.movies$.then((v) => {setMovieList(v)})
+    }
+    loadData();
+  }, [])
+
+  let movieData =  movieList.map((movie) => {
+    var isLiked = false
+    return(<CardMovie  />)
+  })
+
   return (
     <div className="Body">
       <div className="Header">
         <img src={logo} className="logo-top" alt="Logo My Moviz"/>
-        <h1>MOVIES</h1>
+        <h1>MOVIES </h1>
       </div>
       <div className="content">
-        <CardMovie/>
+        {movieData}
       </div>
     </div>
   );
